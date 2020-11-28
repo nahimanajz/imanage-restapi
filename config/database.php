2f8e422cb2bbad24c391b6cad66757aae758a795
nahimanajz <jazzonahimana@gmail.com>
2020-11-28 20:22:25 +0200

    chang mysql db to sqlite database

--- config/database.php
+++ config/database.php
@@ -31,7 +31,7 @@ return [
     | so make sure you have the driver for your particular database of
     | choice installed on your machine before you begin development.
     |
-    */
+    */ 
 
     'connections' => [
 
